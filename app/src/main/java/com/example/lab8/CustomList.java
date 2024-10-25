759c1bc300c6399233c1cd1177345dcafbefd7fb
Gabriel Vasquez <gabriel.felipe.008@gmail.com>
2024-10-24 23:20:53 -0600

    correct impl of getCount (should pass)

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,8 +46,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount(){
-        //return this.cities.size();
-        return 0;
+        return this.cities.size();
     }
 
     public void addCity(City city){
